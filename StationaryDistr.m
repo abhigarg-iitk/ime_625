bc4e660db538c58721dc68cb0df4bca8d6f0f05d
Anuj Nagpal <anujnagpal96@gmail.com>
2017-04-21 06:52:44 +0530

    Added Functions for calculating periodicity and stationary distribution

---
+++ StationaryDistr.m
@@ -0,0 +1,11 @@
+function [statdistr] = StationaryDistr(transition)
+    %transition = [0.5 0.5; 0.4 0.6];
+    transition = transition.';
+    for i=1:size(transition,1)
+        transition(i,i) = transition(i,i) - 1
+    end
+    transition = [transition; 1 1];
+    A = zeros(size(transition,1)-1,1);
+    A = [A; 1];
+    linsolve(transition,A);
+end
